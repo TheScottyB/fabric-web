2f1391a9aed13177282aa2815c5c68024653c0d1
thescottybe <beilsco@gmail.com>
2026-02-17 19:40:41 -0600

    Ignore and untrack SvelteKit build artifacts

--- .svelte-kit/generated/client/nodes/14.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/tags/[tag]/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/tags/[tag]/+page.svelte";
